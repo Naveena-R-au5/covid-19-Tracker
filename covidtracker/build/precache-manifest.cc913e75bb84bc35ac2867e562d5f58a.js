056039c8cb9da4a136ba93484f30fc771af2fd36
Naveena-sharma <naveenar@iccsmail.com>
2021-11-03 12:50:59 +0530

    added build

---
+++ covidtracker/build/precache-manifest.cc913e75bb84bc35ac2867e562d5f58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca8840a6a164c4e094563666c644c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa302881604373727041",
+    "url": "/static/css/main.f149fadd.chunk.css"
+  },
+  {
+    "revision": "f7a41793181b1a1e801c",
+    "url": "/static/js/2.8f6f9aae.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.8f6f9aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa302881604373727041",
+    "url": "/static/js/main.a86429a4.chunk.js"
+  },
+  {
+    "revision": "8862016860622b01e6aa",
+    "url": "/static/js/runtime-main.f22b2772.js"
+  },
+  {
+    "revision": "4e5f2f9b8bcff49222d79234983146b8",
+    "url": "/static/media/cc.4e5f2f9b.jpg"
+  }
+]);
